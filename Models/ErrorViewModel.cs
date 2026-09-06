36fe508eb875ba0a8b3061cc694c5de771f34f30
phangiahuy271105 <phangiahuy271105@gmail.com>
2026-09-06 23:33:10 +0700

    update code

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DoAn_Pc_DACS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
